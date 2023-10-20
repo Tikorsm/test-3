842acc8bd6cd2b657dbe9a2f31f094dbf7de5975
Your Name <you@example.com>
2023-10-20 06:58:57 -0300

    commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Main } from '@/components/Main'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
